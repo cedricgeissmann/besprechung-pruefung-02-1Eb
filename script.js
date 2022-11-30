44d7ded04dd7872a807bfa68e74f14252a85e77f
Cedric Geissmann <cedric.geissmann@gmail.com>
2022-11-30 15:10:08 +0100

    5d

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ function right() {
     cx = cx + 50
     blub.setAttribute("cx", cx)
 }
+
+function left() {
+    const blub = document.querySelector("#blubber")
+    let cx = parseInt(blub.getAttribute("cx"))
+    cx = cx - 50
+    blub.setAttribute("cx", cx)
+}
+
+function up() {
+    const blub = document.querySelector("#blubber")
+    let cy = parseInt(blub.getAttribute("cy"))
+    cy = cy - 50
+    blub.setAttribute("cy", cy)
+}
+
+function down() {
+    const blub = document.querySelector("#blubber")
+    let cy = parseInt(blub.getAttribute("cy"))
+    cy = cy + 50
+    blub.setAttribute("cy", cy)
+}
